02479741edb0f539cf69955a4cb01eb54be81596
sayongalamulume06-png <sayongalamulume06@gmail.com>
2026-07-13 22:27:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const { ExpressPeerServer } = require('peer');
 const http = require('http');
+const path = require('path'); // Nécessaire pour localiser le fichier html
 
 const server = http.createServer(app);
 const peerServer = ExpressPeerServer(server, {
@@ -10,8 +11,9 @@ const peerServer = ExpressPeerServer(server, {
 
 app.use('/peerjs', peerServer);
 
+// Sert le fichier index.html quand on visite la page d'accueil
 app.get('/', (req, res) => {
-    res.send('Bienvenue sur Mbidi Rencontre ! Le serveur est prêt pour les appels.');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
